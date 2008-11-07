1cbbc00b8e037c37265137dd6828bf877931066d
ian@caret.cam.ac.uk <ian@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-11-07 18:32:11 +0000

    Webdav server implementation. (yes this is complete!)
    git-svn-id: https://source.sakaiproject.org/contrib/k2/trunk@54720 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ webdav/src/main/java/org/sakaiproject/webdav/SakaiSessionProvider.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.webdav;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jackrabbit.server.SessionProvider;
+import org.sakaiproject.kernel.api.jcr.JCRService;
+
+import javax.jcr.LoginException;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 
+ */
+public class SakaiSessionProvider implements SessionProvider {
+  private static final Log LOG = LogFactory.getLog(SakaiSessionProvider.class);
+  private JCRService jcrService;
+
+  /**
+   * @param jcrService
+   */
+  public SakaiSessionProvider(JCRService jcrService) {
+    this.jcrService = jcrService;
+  }
+
+  public Session getSession(HttpServletRequest request, Repository rep,
+      String workspace) throws LoginException {
+
+    // we do not accept the anonymous session for WebDAV !
+    if (request.getAuthType() == null) {
+      throw new LoginException("Authentication required for WebDAV");
+    }
+
+    try {
+      return jcrService.login();
+    } catch (RepositoryException e) {
+      throw new LoginException("Authentication for WebDAV, failed ", e);
+    }
+  }
+
+  public void releaseSession(Session session) {
+    try {
+      jcrService.logout();
+    } catch (LoginException e) {
+      LOG.error("Failed to logout ", e);
+    } catch (RepositoryException e) {
+      LOG.error("Failed to logout ", e);
+    }
+  }
+
+}
---
+++ webdav/src/main/java/org/sakaiproject/webdav/SakaiWebDavServlet.java
@@ -0,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.webdav;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jackrabbit.server.SessionProvider;
+import org.apache.jackrabbit.webdav.DavLocatorFactory;
+import org.apache.jackrabbit.webdav.simple.ResourceConfig;
+import org.apache.jackrabbit.webdav.simple.SimpleWebdavServlet;
+import org.sakaiproject.kernel.api.KernelManager;
+import org.sakaiproject.kernel.api.jcr.JCRService;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.jcr.Repository;
+import javax.jcr.Session;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This code is based on Apache Sling code (and it should take all credit)
+ */
+public class SakaiWebDavServlet extends SimpleWebdavServlet {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  private static final Log LOG = LogFactory.getLog(SakaiWebDavServlet.class);
+  private Repository repository;
+  private JCRService jcrService;
+
+  @Override
+  public Repository getRepository() {
+    return repository;
+  }
+
+  @Override
+  public void init() throws ServletException {
+    super.init();
+
+    KernelManager km = new KernelManager();
+    jcrService = km.getService(JCRService.class);
+    repository = jcrService.getRepository();
+
+    // for now, the ResourceConfig is fixed
+    final String configPath = "/webdav-resource-config.xml";
+    final ResourceConfig rc = new ResourceConfig();
+    final URL cfg = getClass().getResource(configPath);
+    if (cfg == null) {
+      throw new UnavailableException("ResourceConfig source not found:"
+          + configPath);
+    }
+
+    rc.parse(cfg);
+    setResourceConfig(rc);
+  }
+
+  @Override
+  protected void service(HttpServletRequest request,
+      HttpServletResponse response) throws ServletException, IOException {
+
+    final String pinfo = request.getPathInfo();
+
+    if (pinfo == null || "/".equals(pinfo)) {
+      // redirect to the default workspace if directly addressing the
+      // servlet
+      // and if the default workspace name is not null (in which case we'd
+      // need
+      // to login to find out the actual workspace name, SLING-256)
+      if (jcrService.getDefaultWorkspace() == null) {
+        response
+            .sendError(
+                HttpServletResponse.SC_NOT_FOUND,
+                "JCR workspace name required, please add it to the end of the URL"
+                    + " (for the Jackrabbit embedded repository the default name is 'default') ");
+      } else {
+        String uri = request.getRequestURI();
+        if (pinfo == null) {
+          uri += "/";
+        }
+        uri += jcrService.getDefaultWorkspace();
+        response.sendRedirect(uri);
+      }
+
+    } else {
+
+      super.service(request, response);
+    }
+
+  }
+
+  private DavLocatorFactory locatorFactory;
+
+  private SessionProvider sessionProvider;
+
+  // ---------- SimpleWebdavServlet overwrites -------------------------------
+
+  @Override
+  public DavLocatorFactory getLocatorFactory() {
+    if (locatorFactory == null) {
+
+      String workspace = jcrService.getDefaultWorkspace();
+
+      // no configuration, try to login and acquire the default name
+      if (workspace == null || workspace.length() == 0) {
+        Session tmp = null;
+        try {
+          tmp = jcrService.login();
+          workspace = tmp.getWorkspace().getName();
+        } catch (Throwable t) {
+          LOG.info("Using Fallback workspace ");
+          // TODO: log !!
+          workspace = "default"; // fall back name
+        } finally {
+          if (tmp != null) {
+            tmp.logout();
+          }
+        }
+      }
+
+      locatorFactory = new SakaiLocatorFactory(workspace);
+    }
+    return locatorFactory;
+  }
+
+  @Override
+  public synchronized SessionProvider getSessionProvider() {
+    if (sessionProvider == null) {
+      sessionProvider = new SakaiSessionProvider(jcrService);
+    }
+    return sessionProvider;
+  }
+
+}
