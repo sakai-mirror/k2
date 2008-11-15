0e0176947f06b20f89c6b6782410b8b64c74741f
tpamsler@ucdavis.edu <tpamsler@ucdavis.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-11-15 20:17:16 +0000

    NOJIRA : Adding check for /Users/thomas/local/tomcat in start.sh. If the /Users/thomas/local/tomcat is set, unset it.
    git-svn-id: https://source.sakaiproject.org/contrib/k2/trunk@55016 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh 
+
+if [ -n "$CATALINA_HOME" ]
+then
+  echo "CATALINA_HOME is set, removing it ..."
+  unset CATALINA_HOME
+fi
+
 export SAKAI_KERNEL_PROPERTIES=`pwd`/localkernel.properties
 target/runtime/bin/catalina.sh $*
