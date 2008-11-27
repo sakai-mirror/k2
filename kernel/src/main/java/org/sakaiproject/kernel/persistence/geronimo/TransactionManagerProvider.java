edd7b3b06c148c614fc26dd4a3665185214688cf
ian@caret.cam.ac.uk <ian@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-11-27 23:09:30 +0000

    Contructors that have parameters need to be annotated with the @Inject annotation to tell Guice which on is a constructor.
    git-svn-id: https://source.sakaiproject.org/contrib/k2/trunk@55404 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- kernel/src/main/java/org/sakaiproject/kernel/persistence/geronimo/TransactionManagerProvider.java
+++ kernel/src/main/java/org/sakaiproject/kernel/persistence/geronimo/TransactionManagerProvider.java
@@ -5,6 +5,7 @@ import javax.transaction.xa.XAException;
 
 import org.apache.geronimo.transaction.manager.GeronimoTransactionManager;
 
+import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.name.Named;
 
@@ -12,6 +13,7 @@ public class TransactionManagerProvider implements Provider<TransactionManager> 
   public static final String TRANSACTION_TIMEOUT_SECONDS = "transaction.timeoutSeconds";
   private GeronimoTransactionManager transMgr;
 
+  @Inject
   public TransactionManagerProvider(
       @Named(TRANSACTION_TIMEOUT_SECONDS) int defaultTransactionTimeoutSeconds)
       throws XAException {
