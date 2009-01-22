ae048db28d2c4119e9988642201ae5acd3ddfc8a
ian@caret.cam.ac.uk <ian@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-01-22 14:53:19 +0000

    Reduced binding to impl (spotted by JonG)
    git-svn-id: https://source.sakaiproject.org/contrib/k2/trunk@57229 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- kernel/src/test/java/org/sakaiproject/kernel/test/KernelIntegrationBase.java
+++ kernel/src/test/java/org/sakaiproject/kernel/test/KernelIntegrationBase.java
@@ -28,6 +28,7 @@ import org.sakaiproject.kernel.api.memory.CacheManagerService;
 import org.sakaiproject.kernel.api.memory.CacheScope;
 import org.sakaiproject.kernel.api.session.SessionManagerService;
 import org.sakaiproject.kernel.api.user.UserResolverService;
+import org.sakaiproject.kernel.api.userenv.UserEnvironmentResolverService;
 import org.sakaiproject.kernel.authz.simple.SimpleJcrUserEnvironmentResolverService;
 import org.sakaiproject.kernel.component.KernelLifecycle;
 import org.sakaiproject.kernel.user.jcr.JcrAuthenticationResolverProvider;
@@ -174,7 +175,7 @@ public class KernelIntegrationBase {
   public static String getUserEnvPath(String userId) {
     String prefix = PathUtils.getUserPrefix(userId);
     return "/userenv" + prefix
-        + SimpleJcrUserEnvironmentResolverService.USERENV;
+        + UserEnvironmentResolverService.USERENV;
   }
 
   /**
