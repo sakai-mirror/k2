9e858a9f39a16581affb2d827aae0ed3c4575f2d
ian@caret.cam.ac.uk <ian@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-01-28 00:02:29 +0000

    Cleaned up unused imports and variables.
    git-svn-id: https://source.sakaiproject.org/contrib/k2/trunk@57493 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- kernel/src/test/java/org/sakaiproject/kernel/rest/test/RestMySitesProviderTest.java
+++ kernel/src/test/java/org/sakaiproject/kernel/rest/test/RestMySitesProviderTest.java
@@ -17,8 +17,9 @@
  */
 package org.sakaiproject.kernel.rest.test;
 
+import static org.easymock.EasyMock.anyObject;
 import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
@@ -36,7 +37,6 @@ import org.sakaiproject.kernel.api.Registry;
 import org.sakaiproject.kernel.api.RegistryService;
 import org.sakaiproject.kernel.api.authz.AuthzResolverService;
 import org.sakaiproject.kernel.api.jcr.JCRService;
-import org.sakaiproject.kernel.api.jcr.support.JCRNodeFactoryService;
 import org.sakaiproject.kernel.api.jcr.support.JCRNodeFactoryServiceException;
 import org.sakaiproject.kernel.api.memory.CacheManagerService;
 import org.sakaiproject.kernel.api.memory.CacheScope;
@@ -45,16 +45,12 @@ import org.sakaiproject.kernel.api.session.SessionManagerService;
 import org.sakaiproject.kernel.api.user.UserResolverService;
 import org.sakaiproject.kernel.jcr.jackrabbit.sakai.SakaiJCRCredentials;
 import org.sakaiproject.kernel.rest.RestMySitesProvider;
-import org.sakaiproject.kernel.test.AuthZServiceKernelUnitT;
 import org.sakaiproject.kernel.test.KernelIntegrationBase;
-import org.sakaiproject.kernel.util.PathUtils;
-import org.sakaiproject.kernel.util.ResourceLoader;
 import org.sakaiproject.kernel.webapp.SakaiServletRequest;
 import org.sakaiproject.kernel.webapp.test.InternalUser;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
 
@@ -70,7 +66,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * 
+ *
  */
 public class RestMySitesProviderTest extends KernelIntegrationBase {
 
@@ -79,33 +75,11 @@ public class RestMySitesProviderTest extends KernelIntegrationBase {
 
   private static boolean shutdownKernel;
 
-  private static String CREATE_ELEMENT = "create";
-
   @BeforeClass
-  public static void beforeThisClass() throws ComponentActivatorException {
+  public static void beforeThisClass() throws Exception {
     shutdownKernel = KernelIntegrationBase.beforeClass();
-    try {
-      KernelIntegrationBase.loadTestUsers();
-      KernelIntegrationBase.loadTestSites();
-    } catch (AccessDeniedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (NoSuchAlgorithmException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (RepositoryException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (JCRNodeFactoryServiceException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    KernelIntegrationBase.loadTestUsers();
+    KernelIntegrationBase.loadTestSites();
   }
 
   @AfterClass
@@ -114,71 +88,76 @@ public class RestMySitesProviderTest extends KernelIntegrationBase {
   }
 
   @Test
-  public void testUserWithEnv() throws ServletException, IOException, JCRNodeFactoryServiceException, LoginException, RepositoryException {
-    
-    
-    
+  public void testUserWithEnv() throws ServletException, IOException,
+      JCRNodeFactoryServiceException, LoginException, RepositoryException {
+
     KernelManager km = new KernelManager();
-    SessionManagerService sessionManagerService = km.getService(SessionManagerService.class);
-    CacheManagerService cacheManagerService = km.getService(CacheManagerService.class);
-    UserResolverService userResolverService = km.getService(UserResolverService.class);
-    AuthzResolverService authzResolverService = km.getService(AuthzResolverService.class);
+    SessionManagerService sessionManagerService = km
+        .getService(SessionManagerService.class);
+    CacheManagerService cacheManagerService = km
+        .getService(CacheManagerService.class);
+    UserResolverService userResolverService = km
+        .getService(UserResolverService.class);
+    AuthzResolverService authzResolverService = km
+        .getService(AuthzResolverService.class);
     JCRService jcrService = km.getService(JCRService.class);
-    JCRNodeFactoryService jcrNodeFactoryService = km.getService(JCRNodeFactoryService.class);
     // bypass security
     authzResolverService.setRequestGrant("Populating Test JSON");
-    
+
     // login to the repo with super admin
     SakaiJCRCredentials credentials = new SakaiJCRCredentials();
     Session jsession = jcrService.getRepository().login(credentials);
     jcrService.setSession(jsession);
 
     // setup the user environment for the admin user.
-  /*  for (String userName : USERS) {
-      String prefix = PathUtils.getUserPrefix(userName);
-      String userEnvironmentPath = "/userenv" + prefix + "userenv";
+    /*
+     * for (String userName : USERS) { String prefix =
+     * PathUtils.getUserPrefix(userName); String userEnvironmentPath =
+     * "/userenv" + prefix + "userenv";
+     *
+     * LOG.info("Saving "+userEnvironmentPath);
+     * jcrNodeFactoryService.createFile(userEnvironmentPath); InputStream in =
+     * ResourceLoader.openResource(USERBASE + userName + ".json",
+     * RestMySitesProviderTest.class.getClassLoader());
+     * jcrNodeFactoryService.setInputStream(userEnvironmentPath, in);
+     * jsession.save(); in.close(); }
+     */
 
-      LOG.info("Saving "+userEnvironmentPath);
-      jcrNodeFactoryService.createFile(userEnvironmentPath);
-      InputStream in = ResourceLoader.openResource(USERBASE + userName
-          + ".json", RestMySitesProviderTest.class.getClassLoader());
-      jcrNodeFactoryService.setInputStream(userEnvironmentPath, in);
-      jsession.save();
-      in.close();
-    }*/
-
-    
-    LOG.info("Getting RestMySitesProvider using key: " + RestMySitesProvider.SITES_ELEMENT);
+    LOG.info("Getting RestMySitesProvider using key: "
+        + RestMySitesProvider.SITES_ELEMENT);
     RegistryService registryService = km.getService(RegistryService.class);
-    Registry<String, RestProvider> registry = registryService.getRegistry(RestProvider.REST_REGISTRY);
-    RestMySitesProvider rmsp = (RestMySitesProvider) registry.getMap().get(RestMySitesProvider.SITES_ELEMENT);
-    for(String key:registry.getMap().keySet()){
-    	LOG.info(key + "--->" +registry.getMap().get(key));
+    Registry<String, RestProvider> registry = registryService
+        .getRegistry(RestProvider.REST_REGISTRY);
+    RestMySitesProvider rmsp = (RestMySitesProvider) registry.getMap().get(
+        RestMySitesProvider.SITES_ELEMENT);
+    for (String key : registry.getMap().keySet()) {
+      LOG.info(key + "--->" + registry.getMap().get(key));
     }
     HttpServletRequest request = createMock(HttpServletRequest.class);
     HttpServletResponse response = createMock(HttpServletResponse.class);
     HttpSession session = createMock(HttpSession.class);
-    
-    
-    Assert.assertNotNull(rmsp);
-    
 
-    
-   expect(request.getAttribute("_no_session")).andReturn(null).anyTimes();
-   
+    Assert.assertNotNull(rmsp);
+
+    expect(request.getAttribute("_no_session")).andReturn(null).anyTimes();
+
     expect(request.getSession(true)).andReturn(session).anyTimes();
     expect(request.getAttribute("_uuid")).andReturn(null).anyTimes();
-    expect(session.getAttribute("_u")).andReturn(new InternalUser("ib236")).anyTimes();
+    expect(session.getAttribute("_u")).andReturn(new InternalUser("ib236"))
+        .anyTimes();
     expect(session.getAttribute("_uu")).andReturn(null).anyTimes();
-    expect(request.getLocale()).andReturn(new Locale("en","US")).anyTimes();
+    expect(request.getLocale()).andReturn(new Locale("en", "US")).anyTimes();
     expect(session.getAttribute("sakai.locale.")).andReturn(null).anyTimes();
-    expect(request.getParameter(RestMySitesProvider.INPUT_PARAM_NAME_STARTINDEX)).andReturn(null).anyTimes();
-    expect(request.getParameter(RestMySitesProvider.INPUT_PARAM_NAME_COUNT)).andReturn(null).anyTimes();
+    expect(
+        request.getParameter(RestMySitesProvider.INPUT_PARAM_NAME_STARTINDEX))
+        .andReturn(null).anyTimes();
+    expect(request.getParameter(RestMySitesProvider.INPUT_PARAM_NAME_COUNT))
+        .andReturn(null).anyTimes();
     expect(request.getRequestedSessionId()).andReturn("sessionId").anyTimes();
-    
+
     Cookie c = new Cookie("JSESSIONID", "sessionId");
-    
-    expect(request.getCookies()).andReturn(new Cookie[] {c}).anyTimes();
+
+    expect(request.getCookies()).andReturn(new Cookie[] { c }).anyTimes();
     expect(session.getId()).andReturn("sessionId").anyTimes();
     response.addCookie((Cookie) anyObject());
     expectLastCall().atLeastOnce();
@@ -191,26 +170,27 @@ public class RestMySitesProviderTest extends KernelIntegrationBase {
       public void write(int b) throws IOException {
         baos.write(b);
       }
-      
+
     });
     expectLastCall().atLeastOnce();
-      replay(request,response,session);
-    
-    SakaiServletRequest sakaiServletRequest = new SakaiServletRequest(request, response, 
-    		userResolverService, sessionManagerService);
-    sessionManagerService.bindRequest(sakaiServletRequest);
-    
-    LOG.info("Dispatching to provider... /rest/" + RestMySitesProvider.SITES_ELEMENT);
-    rmsp.dispatch(new String[] {RestMySitesProvider.SITES_ELEMENT}, request, response);
+    replay(request, response, session);
 
-    String responseString = new String(baos.toByteArray(),"UTF-8");
-    LOG.info("Response was "+responseString );
+    SakaiServletRequest sakaiServletRequest = new SakaiServletRequest(request,
+        response, userResolverService, sessionManagerService);
+    sessionManagerService.bindRequest(sakaiServletRequest);
+
+    LOG.info("Dispatching to provider... /rest/"
+        + RestMySitesProvider.SITES_ELEMENT);
+    rmsp.dispatch(new String[] { RestMySitesProvider.SITES_ELEMENT }, request,
+        response);
+
+    String responseString = new String(baos.toByteArray(), "UTF-8");
+    LOG.info("Response was " + responseString);
     assertTrue(responseString.indexOf("\"entry\"") > 0);
-    
-    
+
     cacheManagerService.unbind(CacheScope.REQUEST);
-    verify(request,response,session);
-    
+    verify(request, response, session);
+
   }
 
 }
