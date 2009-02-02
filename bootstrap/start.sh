0822fc54d8474a9f173ac895a3702c03efd6e285
ian@caret.cam.ac.uk <ian@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-02-02 13:21:00 +0000

    Added Settings for yourkit profilling.
    git-svn-id: https://source.sakaiproject.org/contrib/k2/trunk@57653 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -12,5 +12,8 @@ then
 fi
 echo "SAKAI_KERNEL_PROPERTIES is set to $SAKAI_KERNEL_PROPERTIES"
 
+#  for YourKit export JAVA_OPTS="-server -Dcom.sun.management.jmxremote  -Djava.awt.headless=true -agentlib:yjpagent "
+
+
 
 target/runtime/bin/catalina.sh $*
