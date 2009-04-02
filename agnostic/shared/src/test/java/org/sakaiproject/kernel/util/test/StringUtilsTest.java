8985eb3e578c0cde15cf9ef08a34b587e5ac7fd5
carl.hall@et.gatech.edu <carl.hall@et.gatech.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-04-02 21:15:20 +0000

    Combined StringUtilsTests and removed redundant one from kernel.
    git-svn-id: https://source.sakaiproject.org/contrib/k2/trunk@59562 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- agnostic/shared/src/test/java/org/sakaiproject/kernel/util/test/StringUtilsTest.java
+++ agnostic/shared/src/test/java/org/sakaiproject/kernel/util/test/StringUtilsTest.java
@@ -17,7 +17,9 @@
  */
 package org.sakaiproject.kernel.util.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.sakaiproject.kernel.util.StringUtils;
@@ -26,9 +28,20 @@ import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * 
+ *
  */
 public class StringUtilsTest {
+  @Test
+  public void splitEmpty() {
+    String[] split = StringUtils.split(null, '/');
+    assertEquals(0, split.length);
+
+    split = StringUtils.split("", '/');
+    assertEquals(0, split.length);
+
+    split = StringUtils.split("/", '/');
+    assertEquals(0, split.length);
+  }
 
   /**
    * Test method for
@@ -37,7 +50,7 @@ public class StringUtilsTest {
    */
   @Test
   public void testSplit() {
-    
+
     String[] e = StringUtils
         .split("a.test.of.something.that.should.be.ok", '.');
     assertEquals(8, e.length);
@@ -50,7 +63,7 @@ public class StringUtilsTest {
     assertEquals("be", e[6]);
     assertEquals("ok", e[7]);
     e = StringUtils.split("", '.');
-    assertEquals(0,e.length);
+    assertEquals(0, e.length);
     e = StringUtils.split(".a.test.of.something.that.should.be.ok", '.');
     assertEquals(8, e.length);
     assertEquals("a", e[0]);
@@ -82,18 +95,18 @@ public class StringUtilsTest {
     assertEquals("be", e[6]);
     assertEquals("ok", e[7]);
     e = StringUtils.split(".......that.......", '.');
-    assertEquals(1, e.length);
-    assertEquals("that", e[0]);
+    assertEquals(13, e.length);
+    assertEquals("that", e[6]);
     e = StringUtils.split(".......that..is.....", '.');
-    assertEquals(3, e.length);
-    assertEquals("that", e[0]);
-    assertEquals("", e[1]);
-    assertEquals("is", e[2]);
+    assertEquals(13, e.length);
+    assertEquals("that", e[6]);
+    assertEquals("", e[7]);
+    assertEquals("is", e[8]);
   }
 
   public void testSplitLimit() {
-    String[] e = StringUtils
-        .split("a.test.of.something.that.should.be.ok", '.',100);
+    String[] e = StringUtils.split("a.test.of.something.that.should.be.ok",
+        '.', 100);
     assertEquals(8, e.length);
     assertEquals("a", e[0]);
     assertEquals("test", e[1]);
@@ -103,17 +116,17 @@ public class StringUtilsTest {
     assertEquals("should", e[5]);
     assertEquals("be", e[6]);
     assertEquals("ok", e[7]);
-    e = StringUtils.split(".a.test.of.something.that.should.be.ok", '.',4);
+    e = StringUtils.split(".a.test.of.something.that.should.be.ok", '.', 4);
     assertEquals(4, e.length);
     assertEquals("a", e[0]);
     assertEquals("test", e[1]);
     assertEquals("of", e[2]);
     assertEquals("something", e[3]);
-    e = StringUtils.split(".a.test.of.something.that.should.be.ok.", '.',2);
+    e = StringUtils.split(".a.test.of.something.that.should.be.ok.", '.', 2);
     assertEquals(2, e.length);
     assertEquals("a", e[0]);
     assertEquals("test", e[1]);
-    e = StringUtils.split("a.test.of.something.that.should.be.ok.", '.',8);
+    e = StringUtils.split("a.test.of.something.that.should.be.ok.", '.', 8);
     assertEquals(8, e.length);
     assertEquals("a", e[0]);
     assertEquals("test", e[1]);
@@ -123,43 +136,133 @@ public class StringUtilsTest {
     assertEquals("should", e[5]);
     assertEquals("be", e[6]);
     assertEquals("ok", e[7]);
-    e = StringUtils.split(".......that.......", '.',5);
+    e = StringUtils.split(".......that.......", '.', 5);
     assertEquals(1, e.length);
     assertEquals("that", e[0]);
-    e = StringUtils.split(".......that..is.....", '.',2);
+    e = StringUtils.split(".......that..is.....", '.', 2);
     assertEquals(2, e.length);
     assertEquals("that", e[0]);
     assertEquals("", e[1]);
-    e = StringUtils.split("..............", '.',2);
+    e = StringUtils.split("..............", '.', 2);
     assertEquals(0, e.length);
-    
-//    StringUtils.byteToHex(base);
-//    StringUtils.sha1Hash(tohash);
+
+    // StringUtils.byteToHex(base);
+    // StringUtils.sha1Hash(tohash);
   }
-  
+
   @Test
   public void testByteToHex() {
-   for ( int i = 0; i < 256; i++) {
-     
-     byte[] b = new byte[10];
-     for ( int j = 0; j < b.length; j++) {
-       b[j] = (byte) (i-128);
-     }
-     StringUtils.byteToHex(b);
-   }
-  }
-  
-  @Test
-  public void testSha1Hash() throws UnsupportedEncodingException, NoSuchAlgorithmException {
-    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils.sha1Hash("password"));
-    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils.sha1Hash("password"));
-    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils.sha1Hash("password"));
-    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils.sha1Hash("password"));
-    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils.sha1Hash("password"));
-    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils.sha1Hash("password"));
-    assertTrue(!"db2ae1644939bfbf8602a58bec78b39bfe660f58".equals(StringUtils.sha1Hash("password22")));
-  }
-  
-  
+    for (int i = 0; i < 256; i++) {
 
+      byte[] b = new byte[10];
+      for (int j = 0; j < b.length; j++) {
+        b[j] = (byte) (i - 128);
+      }
+      StringUtils.byteToHex(b);
+    }
+  }
+
+  @Test
+  public void testSha1Hash() throws UnsupportedEncodingException,
+      NoSuchAlgorithmException {
+    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils
+        .sha1Hash("password"));
+    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils
+        .sha1Hash("password"));
+    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils
+        .sha1Hash("password"));
+    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils
+        .sha1Hash("password"));
+    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils
+        .sha1Hash("password"));
+    assertEquals("db2ae1644939bfbf8602a58bec78b39bfe660f58", StringUtils
+        .sha1Hash("password"));
+    assertTrue(!"db2ae1644939bfbf8602a58bec78b39bfe660f58".equals(StringUtils
+        .sha1Hash("password22")));
+  }
+
+  @Test
+  public void join() {
+    String str = " one two three";
+    String join = StringUtils.join(new String[] { "one", "two", "three" }, 0,
+        ' ');
+    assertEquals(str, join);
+
+    join = StringUtils.join(StringUtils.split(str, ' '), 0, ' ');
+    assertEquals(str, join);
+
+    join = StringUtils.join(StringUtils.split(str, ' '), 1, ' ');
+    assertEquals(" two three", join);
+  }
+
+  @Test
+  public void addString() {
+    String[] arr = { "one", "two" };
+    arr = StringUtils.addString(arr, "three");
+    assertEquals(3, arr.length);
+    assertEquals("one", arr[0]);
+    assertEquals("two", arr[1]);
+    assertEquals("three", arr[2]);
+  }
+
+  @Test
+  public void addSameString() {
+    String[] arr = { "one", "two" };
+    arr = StringUtils.addString(arr, "one");
+    assertEquals(2, arr.length);
+    assertEquals("one", arr[0]);
+    assertEquals("two", arr[1]);
+  }
+
+  @Test
+  public void removeStringFromMiddle() {
+    String[] arr = { "one", "two", "three" };
+    arr = StringUtils.removeString(arr, "two");
+    assertEquals(2, arr.length);
+    assertEquals("one", arr[0]);
+    assertEquals("three", arr[1]);
+  }
+
+  @Test
+  public void removeStringFromBeginning() {
+    String[] arr = { "one", "two", "three" };
+    arr = StringUtils.removeString(arr, "one");
+    assertEquals(2, arr.length);
+    assertEquals("two", arr[0]);
+    assertEquals("three", arr[1]);
+  }
+
+  @Test
+  public void removeStringFromEnd() {
+    String[] arr = { "one", "two", "three" };
+    arr = StringUtils.removeString(arr, "three");
+    assertEquals(2, arr.length);
+    assertEquals("one", arr[0]);
+    assertEquals("two", arr[1]);
+  }
+
+  @Test
+  public void removeMissingString() {
+    String[] arr = { "one", "two", "three" };
+    arr = StringUtils.removeString(arr, "four");
+    assertEquals(3, arr.length);
+    assertEquals("one", arr[0]);
+    assertEquals("two", arr[1]);
+    assertEquals("three", arr[2]);
+  }
+
+  @Test
+  public void isEmpty() {
+    assertTrue(StringUtils.isEmpty(""));
+    assertTrue(StringUtils.isEmpty("  "));
+    assertTrue(StringUtils.isEmpty(null));
+    assertFalse(StringUtils.isEmpty("something"));
+    assertFalse(StringUtils.isEmpty(" something "));
+  }
+
+  @Test
+  public void stripBlanks() {
+    assertEquals("thisisatest", StringUtils.stripBlanks("this is a test"));
+    assertEquals("thisisatest", StringUtils.stripBlanks(" this isa test "));
+  }
 }
